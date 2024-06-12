3fbf3b3ae699880720402f6173e2189a21809cca
ashmin-bhujel <ashminbhujel01@gmail.com>
2024-06-12 20:02:00 +0545

    Add extra products and minor changes in footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,12 @@ export default function Footer() {
       <section className="bg-dark py-10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
           <div className="text-center">
-            <p className="font-barlow text-2xl font-light text-lightMid sm:text-3xl md:text-3xl">
+            <div className="font-barlow text-2xl font-light text-lightMid sm:text-3xl md:text-3xl">
               <div className="flex items-center gap-2">
                 <FaApple />
                 iBitronics
               </div>
-            </p>
+            </div>
             <p className="my-2 text-xs sm:text-sm text-lightMid">
               Made by ANSR
             </p>
